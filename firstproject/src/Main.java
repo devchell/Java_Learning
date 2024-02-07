3ef462c9e619ed1b2b875d7cb388cbae43c605f9
devchell <devchell@outlook.com>
2024-02-07 16:57:49 -0300

    First Project Commit

---
+++ firstproject/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("This is Screen Match!\n");
+
+        int year;
+        int classification;
+        double average;
+        String movie;
+        boolean premium;
+
+        //Cars
+        year = 2006;
+        movie = "Cars";
+        premium = false;
+        average = (7.5 + 7 + 9.5 + 10 + 10 + 9);
+        classification = (int) (average/6);
+        System.out.println("Movie: " + movie);
+        System.out.println("Year: " + year);
+        System.out.println("Classification: " + classification + " Stars");
+        System.out.println("Premium Plan: " + premium);
+    }
+}
